fb7a268fa1ff42068314a22cfde5f6f68ff4a906
Mini Model <minimodel@metamodel.me>
2014-12-14 12:16:26 +0100

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_combinedvalues/languages/ru/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_combinedvalues/languages/ru/tl_metamodel_attribute.php
@@ -8,17 +8,16 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-10-26T04:12:23+02:00
+ * last-updated: 2014-05-20T12:16:23+02:00
  */
-
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['0']    = 'Поля';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_fields']['1']    = 'Выберите один или несколько атрибутов для объединения.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['0']    = 'Форматы';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['1']    = 'Используйте "%s" или любой другой заполнитель (placeholder ) для создания форматированного текста. Смотрите <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a> для получения дополнительной информации.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['combinedvalues_format']['1']    = 'Используйте "%s" или любой другой заполнитель (placeholder) для создания форматированного текста. Смотрите <a href="http://php.net/sprintf" onclick="window.open(this.href)">sprintf</a> для получения дополнительной информации.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']               = 'Атрибуты';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['0']     = 'Принудительно регенерировать';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['1']     = 'Выберите, для комбинирования значений каждый раз при изменении любого из подчиненных полей.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_combinedvalues']['1']     = 'Выберите, для обновления комбинированных значений каждый раз при изменении любого из подчиненных полей.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['attributes']   = 'Атрибуты';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['id']           = 'ID';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['meta']         = 'Метаполя';
